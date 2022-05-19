a4a08b41cdc1db10e481afa786f96ecb13a74aca
Nigel Reoch <nigelreoch@hotmail.com>
2022-05-18 21:10:02 -0400

    change

--- dist/src/lib/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ListingType = void 0;
-var ListingType;
-(function (ListingType) {
-    ListingType["Apartment"] = "APARTMENT";
-    ListingType["House"] = "HOUSE";
-})(ListingType = exports.ListingType || (exports.ListingType = {}));
